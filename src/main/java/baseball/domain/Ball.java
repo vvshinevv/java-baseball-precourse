c60878bbd4fa840f1fcfd714ee1644f482868aac
최홍희 <vvshinevv@gmail.com>
2021-10-06 16:49:02 +0900

    feat : Ball 객체에 validation 추가

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -1,13 +1,31 @@
 package baseball.domain;
 
+import baseball.exception.PlayBallIllegalArgumentException;
+
 import java.util.Objects;
 
+import static baseball.exception.ExceptionMessage.INVALID_PLAY_BALL_NUMBER_MESSAGE;
+
 public class Ball {
+
+    public final static Integer MIN_OF_BALL_NUMBER = 1;
+    public final static Integer MAX_OF_BALL_NUMBER = 9;
 
     private final Integer ball;
 
     private Ball(Integer ball) {
+        validate(ball);
         this.ball = ball;
+    }
+
+    private void validate(Integer ball) {
+        if (!isValidBallNumber(ball)) {
+            throw new PlayBallIllegalArgumentException(INVALID_PLAY_BALL_NUMBER_MESSAGE);
+        }
+    }
+
+    private Boolean isValidBallNumber(Integer ball) {
+        return ball >= MIN_OF_BALL_NUMBER && ball <= MAX_OF_BALL_NUMBER;
     }
 
     public static Ball of(Integer ball) {
