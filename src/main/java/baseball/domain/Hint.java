1c7c922450be6f08b70849308582b515897ff7fa
최홍희 <vvshinevv@gmail.com>
2021-10-06 17:48:48 +0900

    feat : Hint Enum calculateCount 메소드 추가
    사용자가 입련한 부분에 대해서 힌트를 줄 수 있도록 작업
    힌트는 Hint 클래스의 비지니스로직을 가지고 있어서 Hint클래스에 작성

--- src/main/java/baseball/domain/Hint.java
+++ src/main/java/baseball/domain/Hint.java
@@ -1,10 +1,28 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Hint {
 
-    STRIKE("스트라이크"),
-    BALL("볼"),
-    NOTHING("낫싱");
+    STRIKE("스트라이크") {
+        @Override
+        public Integer calculateCount(Balls myBalls, Balls yourBalls) {
+            return null;
+        }
+    },
+    BALL("볼") {
+        @Override
+        public Integer calculateCount(Balls myBalls, Balls yourBalls) {
+            return null;
+        }
+    },
+    NOTHING("낫싱") {
+        @Override
+        public Integer calculateCount(Balls myBalls, Balls yourBalls) {
+            return null;
+        }
+    };
 
     private final String description;
 
@@ -14,5 +32,16 @@ public enum Hint {
 
     public String getDescription() {
         return description;
+    }
+
+    public abstract Integer calculateCount(Balls myBalls, Balls yourBalls);
+
+    public static HintResults calculateResults(Balls myBalls, Balls yourBalls) {
+        List<HintResult> hintResults = new ArrayList<>();
+        for (Hint hint : Hint.values()) {
+            HintResult hintResult = HintResult.of(hint, hint.calculateCount(myBalls, yourBalls));
+            hintResults.add(hintResult);
+        }
+        return HintResults.of(hintResults);
     }
 }
--- src/main/java/baseball/service/BaseBallGame.java
+++ src/main/java/baseball/service/BaseBallGame.java
@@ -3,6 +3,11 @@ package baseball.service;
 import baseball.domain.Balls;
 import baseball.domain.Computer;
 import baseball.domain.GamePlayStatus;
+import baseball.domain.Hint;
+import baseball.domain.HintResult;
+import baseball.domain.HintResults;
+import baseball.domain.User;
+import baseball.io.BaseBallDisplay;
 
 import java.util.ArrayList;
 
@@ -12,6 +17,32 @@ public class BaseBallGame {
         GamePlayStatus status = GamePlayStatus.START;
         while (!status.equals(GamePlayStatus.END)) {
             Computer computer = Computer.of(Balls.createRandomBalls());
+            HintResults hintResults = HintResults.of(new ArrayList<>());
+            userPlayBallGameWithComputer(hintResults, computer);
+            status = BaseBallDisplay.outputOfFinishMessage();
         }
+    }
+
+    private void userPlayBallGameWithComputer(HintResults hintResults, Computer computer) {
+        while (!checkPlayBallStrike(hintResults)) {
+            User user = User.of(BaseBallDisplay.input());
+            hintResults = Hint.calculateResults(user.getBalls(), computer.getBalls());
+            BaseBallDisplay.output(hintResults);
+        }
+    }
+
+    private Boolean checkPlayBallStrike(HintResults hintResults) {
+        Boolean result = Boolean.FALSE;
+        for (HintResult hintResult : hintResults.getHintResults()) {
+            result = checkStrikeBallCount(hintResult);
+
+        }
+        return result;
+    }
+
+    private Boolean checkStrikeBallCount(HintResult hintResult) {
+        return hintResult.getHint().equals(Hint.STRIKE) && hintResult.getCount().equals(Balls.BALLS_SIZE) ?
+                Boolean.TRUE :
+                Boolean.FALSE;
     }
 }
